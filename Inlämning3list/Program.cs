471ef847d2acb5c6f527a4c881095e2793f7c319
majo06002 <Max-Lukas@outlook.com>
2024-02-02 12:42:04 +0100

    Add project files.

---
+++ Inlämning3list/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inlämning3list
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> landsStorlek = new Dictionary<string, int>();
+            landsStorlek["Sverige"] = 450;
+            landsStorlek["Norge"] = 385;
+            landsStorlek["Danmark"] = 43;
+            landsStorlek["Finland"] = 338;
+
+            Console.WriteLine("Vilket land vill du veta storleken av?");
+            string land = Console.ReadLine();
+            Console.WriteLine($"{land}s storlek är {landsStorlek[land]} hundratusentals kvadratkilometer.");
+        }
+    }
+}
